cf5cb36fd9124c6fe1bb350c54f6b9101532664a
abaxter <Adam Baxter>
2023-12-11 17:14:07 +0000

    Moved out ids

---
+++ src/common/ids.rs
@@ -0,0 +1,69 @@
+use bytemuck::{Pod, Zeroable};
+use std::fmt::{Display, Formatter};
+use std::num::NonZeroU64;
+use std::ops::{Add, AddAssign, Sub, SubAssign};
+
+macro_rules! id {
+  ( $x:ident ) => {
+    #[repr(C)]
+    #[derive(Default, Debug, Copy, Clone, PartialOrd, PartialEq, Ord, Eq, Hash, Pod, Zeroable)]
+    pub struct $x(pub(crate) u64);
+
+    impl Display for $x {
+      fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+      }
+    }
+
+    impl From<u64> for $x {
+      #[inline(always)]
+      fn from(value: u64) -> Self {
+        $x(value)
+      }
+    }
+
+    impl From<$x> for u64 {
+      #[inline(always)]
+      fn from(value: $x) -> Self {
+        value.0
+      }
+    }
+
+    impl Add<u64> for $x {
+      type Output = $x;
+
+      fn add(self, rhs: u64) -> Self::Output {
+        $x(self.0 + rhs)
+      }
+    }
+
+    impl Sub<u64> for $x {
+      type Output = $x;
+
+      fn sub(self, rhs: u64) -> Self::Output {
+        $x(self.0 - rhs)
+      }
+    }
+
+    impl AddAssign<u64> for $x {
+      fn add_assign(&mut self, rhs: u64) {
+        self.0 += rhs;
+      }
+    }
+
+    impl SubAssign<u64> for $x {
+      fn sub_assign(&mut self, rhs: u64) {
+        self.0 -= rhs;
+      }
+    }
+
+    impl PartialEq<$x> for u64 {
+      fn eq(&self, other: &$x) -> bool {
+        *self == other.0
+      }
+    }
+  };
+}
+
+id!(PgId);
+id!(TxId);
--- src/common/mod.rs
+++ src/common/mod.rs
@@ -17,106 +17,11 @@ pub mod selfowned;
 pub mod tree;
 pub mod utility;
 
+pub mod ids;
+
+pub use ids::{PgId, TxId};
+
 pub(crate) const ZERO_PGID: PgId = PgId(0);
-
-//TODO: Look up math support
-#[repr(C)]
-#[derive(Default, Debug, Copy, Clone, PartialOrd, PartialEq, Ord, Eq, Hash, Pod, Zeroable)]
-pub struct PgId(pub(crate) u64);
-
-impl Display for PgId {
-  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    write!(f, "{}", self.0)
-  }
-}
-
-impl From<u64> for PgId {
-  #[inline(always)]
-  fn from(value: u64) -> Self {
-    PgId(value)
-  }
-}
-
-impl From<PgId> for u64 {
-  #[inline(always)]
-  fn from(value: PgId) -> Self {
-    value.0
-  }
-}
-
-impl Add<u64> for PgId {
-  type Output = PgId;
-
-  fn add(self, rhs: u64) -> Self::Output {
-    PgId(self.0 + rhs)
-  }
-}
-
-impl Sub<u64> for PgId {
-  type Output = PgId;
-
-  fn sub(self, rhs: u64) -> Self::Output {
-    PgId(self.0 - rhs)
-  }
-}
-
-impl AddAssign<u64> for PgId {
-  fn add_assign(&mut self, rhs: u64) {
-    self.0 += rhs;
-  }
-}
-
-impl SubAssign<u64> for PgId {
-  fn sub_assign(&mut self, rhs: u64) {
-    self.0 -= rhs;
-  }
-}
-
-impl PartialEq<PgId> for u64 {
-  fn eq(&self, other: &PgId) -> bool {
-    *self == other.0
-  }
-}
-
-#[repr(C)]
-#[derive(Default, Debug, Copy, Clone, PartialOrd, PartialEq, Ord, Eq, Hash, Pod, Zeroable)]
-pub struct TxId(pub(crate) u64);
-
-impl Display for TxId {
-  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    write!(f, "{}", self.0)
-  }
-}
-
-impl From<u64> for TxId {
-  #[inline(always)]
-  fn from(value: u64) -> Self {
-    TxId(value)
-  }
-}
-
-impl From<TxId> for u64 {
-  #[inline(always)]
-  fn from(value: TxId) -> Self {
-    value.0
-  }
-}
-
-impl Add<u64> for TxId {
-  type Output = TxId;
-
-  fn add(self, rhs: u64) -> Self::Output {
-    TxId(self.0 + rhs)
-  }
-}
-
-impl Sub<u64> for TxId {
-  type Output = TxId;
-
-  fn sub(self, rhs: u64) -> Self::Output {
-    TxId(self.0 - rhs)
-  }
-}
 
 pub type HashMap<'tx, K, V> = hashbrown::HashMap<K, V, DefaultHashBuilder, &'tx Bump>;
 
